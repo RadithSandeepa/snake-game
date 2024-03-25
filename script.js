a0f7861df7b07315edd2c9881f8b43e9f46e5908
RadithSandeepa <radithsandeepa500@gmail.com>
2024-03-25 13:01:01 +0530

    Final Version

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 //Define HTML elements
 const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const highScoreText = document.getElementById('highScore');
+const gameStart = document.getElementById('gameStart');
+const gameOver = document.getElementById('gameOver');
+const gameBonus = document.getElementById('gameBonus');
+const Theme = document.getElementById('Theme');
 
 //Define game variables
 const gridSize = 20;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
-let direction = 'down';
-let gameInterval;;
+let highScore  = 0;
+let direction = 'right';
+let gameInterval;
 let gameSpeedDelay = 200;
+let gameStarted = false;
+
 
 //Draw game map, snake and food
 function draw() {
     board.innerHTML = '';
     drawSnake();
     drawFood();
+    updateScore();
 }
 
 //Draw snake
@@ -43,10 +55,25 @@ function setPosition(element, position){
 
 //Draw food function
 function drawFood(){
-    const foodElement = createGameElement('div', 'food');
-    setPosition(foodElement, food);
-    board.appendChild(foodElement);
+    if(gameStarted){
+        ///////remove this if there is an error
+        let foodPosition;
+        foodPosition = generateFood();
+        while (isFoodOnSnake(foodPosition)) {
+            foodPosition = generateFood();
+        } 
+        ///////
+        const foodElement = createGameElement('div', 'food');
+        setPosition(foodElement, food);
+        board.appendChild(foodElement);
+    }
 }
+
+//Check if food is on snake's body
+function isFoodOnSnake(foodPosition){
+    return snake.some(segment => segment.x === foodPosition.x && segment.y === foodPosition.y);
+}
+////////
 
 //Generate food
 function generateFood(){
@@ -76,10 +103,13 @@ function move(){
     
     //snake.pop();
     if(head.x === food.x && head.y === food.y){
+        gameBonus.play()
         food = generateFood();
-        clearInterval(); //Clear past interval
+        increaseSpeed();
+        clearInterval(gameInterval); //Clear past interval
         gameInterval = setInterval(() => {
             move();
+            checkCollision();
             draw();
         }, gameSpeedDelay);
     }else{
@@ -93,4 +123,119 @@ function move(){
 //     draw(); //Drawing the new position
 // }, 200)
 
+//Start game function
+function startGame(){
+    gameStart.play()
+    gameStarted = true; //keep track of the running game
+    instructionText.style.display = 'none';
+    logo.style.display = 'none';
+    gameInterval = setInterval(() => {
+        move();
+        checkCollision();
+        draw();
+    }, gameSpeedDelay);
+}
 
+//Keypress event listener
+function handleKeyPress(event){
+    if((!gameStarted && event.code === 'Space') || (!gameStarted && event.key === 'Space')){
+        startGame();
+       
+    }else if(event.key === 'Escape') {
+        togglePause();
+    }else{
+        switch(event.key){
+            case 'ArrowUp':
+                if(direction !== 'down') direction = 'up';
+                break;
+            case 'ArrowRight':
+                if(direction !== 'left') direction = 'right';
+                break;
+            case 'ArrowDown':
+                if(direction !== 'up') direction = 'down';
+                break;
+            case 'ArrowLeft':
+                if(direction !== 'right') direction = 'left';
+                break;
+        }
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress);
+
+function togglePause() {
+    if (gameStarted) {
+        if (gameInterval) {
+            clearInterval(gameInterval);
+            gameInterval = null;
+        } else {
+            gameInterval = setInterval(() => {
+                move();
+                checkCollision();
+                draw();
+            }, gameSpeedDelay);
+        }
+    }
+}
+
+function increaseSpeed(){
+    if(gameSpeedDelay > 150){  
+        gameSpeedDelay -= 5;
+    }else if(gameSpeedDelay > 100){
+        gameSpeedDelay -= 3;
+    }
+    else if(gameSpeedDelay > 50){
+        gameSpeedDelay -= 2;
+    }
+    else if(gameSpeedDelay > 25){
+        gameSpeedDelay -= 1;
+    }
+}
+
+function checkCollision(){
+    const head = snake[0];
+    if(head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize){
+        gameOver.play()
+        resetGame();
+    }
+
+    for(let i = 1; i < snake.length; i++){
+        if(head.x === snake[i].x && head.y === snake[i].y){
+            gameOver.play()
+            resetGame();
+        }
+    }
+}
+
+function resetGame(){
+    updateHighScore();
+    stopGame();
+
+    snake = [{x: 10, y: 10}];
+    food = generateFood();
+    direction = 'right';
+    gameSpeedDelay = 200;
+    updateScore()
+}
+
+function updateScore(){
+    const currentScore = snake.length - 1
+    score.textContent = currentScore.toString().padStart(3, '0');  //make a triple digit score
+}
+
+function stopGame(){
+    clearInterval(gameInterval);
+    gameStarted = false;
+    instructionText.style.display = 'block';
+    logo.style.display = 'block';
+}
+
+function updateHighScore(){
+    const currentScore = snake.length - 1;
+    if(currentScore > highScore){
+        highScore = currentScore;
+        highScoreText.textContent = highScore.toString().padStart(3, '0');
+      
+    }
+    highScoreText.style.display = 'block';
+}   
